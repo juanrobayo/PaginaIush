4507a5079361c7e204af205bfc55621ae33c84e3
JuanR <juanrobayo39@hotmail.com>
2019-08-24 15:20:51 -0500

    cambios u

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Noticias } from 'src/app/components/interfaces/noticias';
 import { tarjeta_proyecto } from '../interfaces/tarjeta_proyecto';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -25,15 +24,13 @@ export class HomeComponent implements OnInit {
       titulo:'Pagina semilleros',
       detalle:'Se crea una nueva pagina para la iush para que sus estudiantes se puedan inscribir a los semilleros y estar actualizados de los proyectos que se estan ejecutando actualmente',
       file:'assets/img/logoSisas.jpeg'
-    }
-
-   
-
+    } 
+  
 
     let proyecto1 :tarjeta_proyecto={
       nombre :'Página Web Del Semillero',
       integrantes:4,
-      texto: "proyecto de prubea test 1111111111111111111111111111111111",
+      texto: "una pagina creada con angular'SPA' para la integracion y  divulgacion de los semilleros de la iush",
       campoobligatorio: true,
       file: "assets/img/proyecto3.jpg"
       };
@@ -41,7 +38,7 @@ export class HomeComponent implements OnInit {
       let proyecto2 :tarjeta_proyecto={
         nombre :'Asistencias Sistematizadas/Automatizadas',
         integrantes:2,
-        texto: "proyecto de  prueba con2222222222222222222222222222222222222",
+        texto: "con el carnet de estudiante registrar las asistencias a lso eventos de la iush",
         campoobligatorio: true,
         file: "assets/img/proyectos1.jpg"
         }
@@ -49,7 +46,7 @@ export class HomeComponent implements OnInit {
       let proyecto3 :tarjeta_proyecto={
         nombre :'Cuadro De Turnos Vigilantes IE De Bello',
         integrantes:3,
-        texto: "proyecto de  prueba con3333333333333333333333333333333",
+        texto: "Proyecto  de generacion de horarios y turnos automaticos para un colegio de Bello",
         campoobligatorio: true,
         file: "assets/img/proyecto2.jpg"
    }
